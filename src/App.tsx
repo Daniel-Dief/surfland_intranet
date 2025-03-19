58052525af62b6e99b161247083f65e9f8cf0acc
Daniel Santos <danieldiefsantos@gmail.com>
2025-03-19 10:34:03 -0300

    feat: auto login

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,32 @@ import { ToastContainer } from 'react-toastify'
 import Home from './templates/Home'
 import MySessions from './templates/MySessions'
 import ModalCase from './components/ModalCase'
+import useToken from './store/useToken'
+import sendLoginRequest from './services/auth/logIn.service'
+import useUser from './store/useUser'
 
 export default function App() {
+  async function handleLogin() {
+    let hashPass = useToken.getState().hashPassword;
+
+    if(hashPass) {
+      const response = await sendLoginRequest(hashPass);
+
+      if(response) {
+        useToken.setState({token: response.token});
+        useUser.setState({
+          userId: response.user.UserId,
+          name: response.user.Name,
+          login: response.user.Login,
+          accessProfileId: response.user.AccessProfileId,
+          statusId: response.user.StatusId
+        });
+      }
+    }
+  }
+
+  handleLogin();
+
   return (
     <Router>
       <ToastContainer position='top-right' autoClose={5000}/>
