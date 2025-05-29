2770754bce9d28896bdd0fe4a41a3dc47f118d59
Daniel Santos <danieldiefsantos@gmail.com>
2025-05-29 12:50:37 -0300

    feat: correção de datas

--- src/components/SheduleCalendar/index.tsx
+++ src/components/SheduleCalendar/index.tsx
@@ -13,7 +13,6 @@ interface Props {
 
 export default function SheduleCalendar({ selectedDate, setSelectedDate, availableDates }: Props) {
     const [disabledDates, setDisabledDates] = useState<Date[]>([]);
-    
     // Configura o mês alvo (próximo mês)
     const targetMonth = new Date();
     targetMonth.setDate(1);
@@ -22,14 +21,14 @@ export default function SheduleCalendar({ selectedDate, setSelectedDate, availab
     useEffect(() => {
         // Cria todas as datas do próximo mês
         const nextMonth = new Date();
-        nextMonth.setDate(1);
-        nextMonth.setMonth(nextMonth.getMonth() + 1);
+        nextMonth.setUTCDate(1);
+        nextMonth.setUTCMonth(nextMonth.getMonth() + 1);
         
-        const year = nextMonth.getFullYear();
-        const month = nextMonth.getMonth();
+        const year = nextMonth.getUTCFullYear();
+        const month = nextMonth.getUTCMonth();
         
         // Último dia do próximo mês
-        const lastDay = new Date(year, month + 1, 0).getDate();
+        const lastDay = new Date(year, month + 1, 0).getUTCDate();
         
         // Array com todas as datas do próximo mês
         const allDatesNextMonth: Date[] = [];
@@ -39,16 +38,14 @@ export default function SheduleCalendar({ selectedDate, setSelectedDate, availab
         
         // Função para verificar se uma data está no array availableDates
         const isDateAvailable = (date: Date) => {
-            return availableDates.some(availableDate => 
-                availableDate.getDate() === date.getDate() &&
-                availableDate.getMonth() === date.getMonth() &&
-                availableDate.getFullYear() === date.getFullYear()
+            return availableDates.some(availableDate =>
+                availableDate.getUTCDate() === date.getUTCDate() &&
+                availableDate.getUTCMonth() === date.getUTCMonth() &&
+                availableDate.getUTCFullYear() === date.getUTCFullYear()
             );
         };
-        
         // Filtra as datas que não estão disponíveis
         const dates = allDatesNextMonth.filter(date => !isDateAvailable(date));
-        
         setDisabledDates(dates);
         const tempElement = document.querySelector(".rdp-caption_label");
         if (tempElement) {
